cc10883c62507124d2fd0248487473f84033404d
olehhapuk <oleggapuk20@gmail.com>
2022-04-02 14:30:01 +0300

    Added tablet support for home page

--- script.js
+++ script.js
@@ -3,14 +3,22 @@ const contactModalElem = document.getElementById('contactModal');
 const closeContactModalBtnElem = document.getElementById('contactModalClose');
 
 function openModal() {
-  contactModalElem.classList.add('modal_open');
+  contactModalElem.style.display = 'block';
+  setTimeout(() => {
+    contactModalElem.classList.add('modal_open');
+  }, 0);
   document.body.style.overflow = 'hidden';
+  document.body.style.marginRight = '17px';
   contactModalElem.querySelector('input').focus();
 }
 
 function closeModal() {
   contactModalElem.classList.remove('modal_open');
-  document.body.style.overflow = 'auto';
+  setTimeout(() => {
+    contactModalElem.style.display = 'none';
+    document.body.style.marginRight = '0';
+    document.body.style.overflow = 'auto';
+  }, 300);
 }
 
 contactBtnElem.addEventListener('click', openModal);
